379559b0fce361fddc4308afcae4115b893015dd
xhb <421271727@qq.com>
2016-01-11 22:14:11 +0800

    EasyUI_01
    Base window

---
+++ EasyUI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EasyUI.Startup))]
+namespace EasyUI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
